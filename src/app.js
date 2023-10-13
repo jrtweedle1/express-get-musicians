76fdf058971c5b3340622e043fec6ea1c6215a92
Michael Vela <mikevela@google.com>
2023-10-13 14:45:27 -0500

    extensions work

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const { Musician } = require("../models/index")
+const { Musician, Band } = require("../models/index")
 const { db } = require("../db/connection")
 
 const port = 3000;
@@ -8,6 +8,30 @@ const port = 3000;
 // Create a GET /musicians route to return all musicians 
 app.get("/musicians", async (request, response) => {
     const data = await Musician.findAll();
+    response.json(data)
+})
+
+app.get("/musicians/1", async (request, response) => {
+    const data = await Musician.findByPk(1);
+    // console.log(data)
+    response.json(data)
+})
+
+app.get("/musicians/2", async (request, response) => {
+    const data = await Musician.findByPk(2);
+    // console.log(data)
+    response.json(data)
+})
+
+app.get("/musicians/3", async (request, response) => {
+    const data = await Musician.findByPk(3);
+    // console.log(data)
+    response.json(data)
+})
+
+app.get("/bands", async (request, response) => {
+    const data = await Band.findAll();
+    console.log(data)
     response.json(data)
 })
 
